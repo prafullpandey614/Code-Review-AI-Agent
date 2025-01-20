05d77cca0c51b91f1e82998226b5bd7e25d44a8e
prafullpandeyOneMind <pkumar1@onemindservices.com>
2025-01-20 11:52:37 +0530

    update req and gitignore

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    CELERY_BROKER_URL: str
-    CELERY_RESULT_BACKEND: str
+    CELERY_BROKER_URL: str = "redis://localhost:6379/0"
+    CELERY_RESULT_BACKEND: str = "redis://localhost:6379/0"
     LLM_API_KEY: str
     GITHUB_API_URL: str = "https://api.github.com"
 
@@ -11,6 +11,3 @@ class Settings(BaseSettings):
         env_file = ".env"  # Specifies the environment file
 
 settings = Settings()
-
-print(settings.CELERY_BROKER_URL)  # This will print the value from the .env file
-print(settings.LLM_API_KEY)       # Prints the API key from the .env file
